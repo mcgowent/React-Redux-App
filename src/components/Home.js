2053f7c7a9cc5bd1fa1202020f3bdb914a534289
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-21 15:55:07 -0700

    Added an h3

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ class Home extends React.Component {
         return (
             <div>
                 <h1>Welcome To the Pokemon Deck Builder</h1>
+                <h3>LOOK AT ME IM UPDATED</h3>
                 <h3>Coming Soon!</h3>
                 <p>Soon you will be able to pick from a few different Trading Card Games and view your decks for each all in one place!</p>
 
